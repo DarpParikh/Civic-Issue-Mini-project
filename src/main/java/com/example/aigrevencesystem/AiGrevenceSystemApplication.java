9b85bfb5f0b10980558d7edc44646e38ed6ff141
Pancham-Ayush <panchamayush@gmail.com>
2026-03-25 08:54:25 +0530

    Initialization

---
+++ src/main/java/com/example/aigrevencesystem/AiGrevenceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.aigrevencesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGrevenceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGrevenceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aigrevencesystem/AiGrevenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aigrevencesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGrevenceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
